f1a7a0050e67851450b2b7fec23d8a1ca9548282
Maciel <joao.maciel@docnix.com.br>
2023-10-14 15:05:07 -0300

    adicionando docs

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,42 +5,76 @@ const tableBuilder = require('../views/assets/js/tableBuilder');
 const uiUpdater = require('../views/assets/js/uiUpdater');
 const fileHandler = require('../models/fileHandler');
 
-ipcRenderer.on('csv-data', (event, data) => {
+// Assinatura do evento 'csv-data' enviado do processo principal
+ipcRenderer.on('csv-data', handleCSVData);
+
+/**
+ * Manipula os dados CSV recebidos do evento 'csv-data'.
+ * @param {Event} event - Evento contendo os dados CSV.
+ * @param {Array} data - Dados do CSV.
+ */
+function handleCSVData(event, data) {
+    // Agrupa os dados por complexidade e cria a tabela e o gráfico
     const complexitiesObject = dataProcessor.groupByComplexity(data);
     tableBuilder.createTableAndChart('myChart', complexitiesObject);
-    calculateTotalItemsAndAverage(data);
-});
 
+    // Calcula o total de itens e a média por dia e atualiza a interface do usuário
+    calculateTotalItemsAndAverage(data);
+}
+
+/**
+ * Calcula o total de itens e a média por dia e atualiza a interface do usuário.
+ * @param {Array} data - Dados do CSV.
+ */
 function calculateTotalItemsAndAverage(data) {
-    const totalItensNoMes = data.length;
+    const totalItemsNoMes = data.length;
+
+    // Cria um mapa contendo a contagem de itens por dia
     const daysMap = csvModel.createDaysMap(data);
 
     const totalDays = daysMap.size;
     const totalItemsPerDay = Array.from(daysMap.values());
     const averageItemsPerDay = csvModel.calculateAverageItemsPerDay(totalItemsPerDay, totalDays);
 
-    uiUpdater.updateUI(totalItensNoMes, averageItemsPerDay);
+    // Atualiza a interface do usuário com os resultados
+    uiUpdater.updateUI(totalItemsNoMes, averageItemsPerDay);
 }
 
+/**
+ * Manipula o evento de recarregar a aplicação.
+ */
 function handleReload() {
     ipcRenderer.send('reload-app');
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+// Aguarda o DOM ser totalmente carregado para adicionar o evento de clique ao botão de upload
+document.addEventListener('DOMContentLoaded', setupEventListeners);
+
+/**
+ * Configura os ouvintes de eventos após o carregamento do DOM.
+ */
+function setupEventListeners() {
     const uploadButton = document.getElementById('uploadButton');
     uploadButton.addEventListener('click', handleUploadButtonClick);
-});
+}
 
+/**
+ * Manipula o clique no botão de upload, abrindo o seletor de arquivos.
+ */
 function handleUploadButtonClick() {
     document.getElementById('fileInput').click();
 }
 
+/**
+ * Manipula o upload de arquivo, verificando se é um arquivo CSV antes de prosseguir.
+ */
 async function handleFileUpload() {
     const fileInput = document.getElementById('fileInput');
     const file = fileInput.files[0];
 
     if (file) {
         if (file.name.endsWith('.csv')) {
+            // Realiza o upload do arquivo CSV
             await fileHandler.uploadCsvFile(file);
         } else {
             alert('Por favor, selecione um arquivo CSV.');
--- src/models/csvModel.js
+++ src/models/csvModel.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const parse = require('csv-parse');
 
+/**
+ * Lê o conteúdo de um arquivo CSV e chama a função de retorno com os dados parseados.
+ * @param {string} filePath - Caminho para o arquivo CSV.
+ * @param {function} callback - Função de retorno que recebe os dados parseados como argumento.
+ */
 function readCSV(filePath, callback) {
     fs.readFile(filePath, 'utf8', (err, fileContent) => {
         if (err) {
@@ -8,34 +13,61 @@ function readCSV(filePath, callback) {
             return;
         }
 
-        parse(fileContent, {
-            columns: true,
-        }, (err, data) => {
-            if (err) {
-                console.error('Erro ao fazer parse do CSV:', err);
-                return;
-            }
-
-            callback(data);
-        });
+        parseCSV(fileContent, callback);
     });
 }
 
+/**
+ * Faz o parse do conteúdo CSV e chama a função de retorno com os dados parseados.
+ * @param {string} csvContent - Conteúdo CSV a ser parseado.
+ * @param {function} callback - Função de retorno que recebe os dados parseados como argumento.
+ */
+function parseCSV(csvContent, callback) {
+    parse(csvContent, { columns: true }, (err, data) => {
+        if (err) {
+            console.error('Erro ao fazer parse do CSV:', err);
+            return;
+        }
+
+        callback(data);
+    });
+}
+
+/**
+ * Cria um mapa contendo a contagem de itens por dia a partir dos dados fornecidos.
+ * @param {Array} data - Dados do CSV parseados.
+ * @returns {Map} - Mapa com a contagem de itens por dia.
+ */
 function createDaysMap(data) {
     return data.reduce((daysMap, entry) => {
         const resolvedDate = new Date(entry['Resolved Date Log1']);
         const dayKey = resolvedDate.toISOString().split('T')[0];
 
-        if (!daysMap.has(dayKey)) {
-            daysMap.set(dayKey, 0);
-        }
-
-        daysMap.set(dayKey, daysMap.get(dayKey) + 1);
+        updateDaysMap(daysMap, dayKey);
 
         return daysMap;
     }, new Map());
 }
 
+/**
+ * Atualiza o mapa de dias com a contagem de itens.
+ * @param {Map} daysMap - Mapa com a contagem de itens por dia.
+ * @param {string} dayKey - Chave do dia.
+ */
+function updateDaysMap(daysMap, dayKey) {
+    if (!daysMap.has(dayKey)) {
+        daysMap.set(dayKey, 0);
+    }
+
+    daysMap.set(dayKey, daysMap.get(dayKey) + 1);
+}
+
+/**
+ * Calcula a média de itens por dia.
+ * @param {Array} totalItemsPerDay - Array contendo a contagem de itens por dia.
+ * @param {number} totalDays - Número total de dias.
+ * @returns {number} - Média de itens por dia.
+ */
 function calculateAverageItemsPerDay(totalItemsPerDay, totalDays) {
     return totalItemsPerDay.reduce((sum, count) => sum + count, 0) / totalDays;
 }
