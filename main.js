90b8b3f314fcd888f8bc2cd2a40e934bdd33dae6
Maciel <joao.maciel@docnix.com.br>
2023-10-15 01:05:02 -0300

    corrigindo bug do vazamento de memoria

--- main.js
+++ main.js
@@ -4,54 +4,71 @@ const { readCSV } = require('./src/models/csvModel');
 
 let mainWindow;
 
+// Função para criar a janela principal
 function createMainWindow(data) {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 1200,
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
+            nodeIntegration: true, // Permite a integração com APIs Node.js no processo de renderização
+            contextIsolation: false, // Desabilita a isolamento de contexto para simplificar o acesso a APIs Node.js
         },
     });
 
     // Carrega o arquivo HTML na janela principal
     mainWindow.loadFile(path.join(__dirname, 'src/views/index.html'));
 
-    // Envia os dados para a janela principal após o carregamento
+    // Envia os dados para a janela principal após o carregamento da página
     mainWindow.webContents.on('did-finish-load', () => {
         mainWindow.webContents.send('csv-data', data);
     });
 
     // Manipula o evento de recarregar o aplicativo
-    ipcMain.on('reload-app', () => {
+    function handleReload() {
         const currentWindow = BrowserWindow.getFocusedWindow();
 
         // Fecha a janela atual se existir
         if (currentWindow) {
             currentWindow.close();
-        }
 
-        // Cria uma nova janela principal com os dados
-        createMainWindow(data);
+            // Adiciona um pequeno atraso antes de criar uma nova janela
+            setTimeout(() => {
+                // Remove todos os ouvintes do evento 'reload-app' antes de adicionar um novo
+                ipcMain.removeAllListeners('reload-app');
+
+                // Cria uma nova janela principal com os dados
+                createMainWindow(data);
+            }, 300); // Ajuste este valor conforme necessário
+        }
+    }
+
+    ipcMain.on('reload-app', handleReload); // Registra o manipulador para o evento 'reload-app'
+
+    // Manipula o evento de fechar a janela
+    mainWindow.on('closed', () => {
+        mainWindow = null;
     });
 
     return mainWindow;
 }
 
 app.whenReady().then(() => {
+    // Lê os dados do arquivo CSV e cria a janela principal
     readCSV(path.join(__dirname, 'dados', 'arquivo.csv'), (data) => {
         createMainWindow(data);
     });
 
+    // Ativa a janela principal se não houver janelas abertas
     app.on('activate', () => {
         if (!mainWindow) {
             createMainWindow();
         }
     });
 
+    // Mantém a aplicação em execução mesmo quando todas as janelas são fechadas
     app.on('window-all-closed', () => {
         if (process.platform !== 'darwin') {
-            app.quit();
+            // Não encerra a aplicação aqui para sistemas não macOS
         }
     });
 });
